653d593b38a29af8c861e36660cfed142d44afde
Mert Cemal Çınar <72888938+mertokaya@users.noreply.github.com>
2022-11-01 23:36:51 +0300

    Add files via upload

---
+++ diğer matematik operatörlerini deneme/diğer matematik operatörlerini deneme.py
@@ -0,0 +1,26 @@
+x=5
+y=8
+print(x/y)
+print(x//y)
+
+#kalan bulma
+print(y%x)
+print(x%y)
+
+#çift ve tek bulma
+print(x%2)
+print(y%2)
+
+#üs ve karakök 
+print(x**2)
+print(y**4)
+
+print(x**(1/2))
+print(y**(1/2))
+
+#işaret değiştirme
+print(x)
+print(-x)
+a=-6
+print(a)
+print(-a)
---
+++ string değişkeni/string değişkeni.py
@@ -0,0 +1,26 @@
+print("mother fucker")
+print("berna sal artık bizi")
+
+eneseözgürlük="berna sal artık boş yapma"
+print(eneseözgürlük)
+
+print('merhaba amklar')
+print("""amk""")
+
+#string indekslerini bulma (stringde 0 dan başlar ekstra not)
+print(eneseözgürlük[0])
+print(eneseözgürlük[-2])
+
+#string değerleri parçalama bölme işlemi
+print(eneseözgürlük[0:7])
+
+#stringde değer atlatma
+print(eneseözgürlük[0:5:2])
+
+print(eneseözgürlük[::2])
+print(eneseözgürlük[::-2])
+
+#string değerlerin birbirleriyle toplanması
+mok="lol"
+print(eneseözgürlük+" "+mok)
+print(eneseözgürlük*3)
